8bd086f19149073f78050463622cf7a0b6772bbe
Michael Bourjac <michael.bourjac@gmail.com>
2023-10-10 21:47:08 +0200

    Add projectTypologyRepository to the IRegistry interface

--- src/sanity/sanity.registry.ts
+++ src/sanity/sanity.registry.ts
@@ -1,9 +1,11 @@
 import { SanityMetadataRepository } from './sanity.metadata';
 import { SanityProjectRepository } from './sanity.project';
+import { SanityProjectTypologyRepository } from './sanity.project-typology';
 import { SanityProjectsPageRepository } from './sanity.projects-page';
 
 export class SanityRegistry {
   projectRepository = new SanityProjectRepository();
   projectsPageRepository = new SanityProjectsPageRepository();
+  projectTypologyRepository = new SanityProjectTypologyRepository();
   metadataRepository = new SanityMetadataRepository();
 }
--- src/types/registry.types.ts
+++ src/types/registry.types.ts
@@ -1,4 +1,5 @@
 import { IMetadata } from '../content/metadata/metadata.types';
+import { IProjectTypology } from '../content/project-typology/project-typology.types';
 import { IProject } from '../content/project/project.types';
 import { IProjectsPage } from '../content/projects-page/projects-page.types';
 import { IRepository } from './repository.types';
@@ -6,5 +7,6 @@ import { IRepository } from './repository.types';
 export interface IRegistry {
   projectRepository: IRepository<IProject>;
   projectsPageRepository: IRepository<IProjectsPage>;
+  projectTypologyRepository: IRepository<IProjectTypology>;
   metadataRepository: IRepository<IMetadata>;
 }
