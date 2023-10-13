9966298281020fdfc786b2d405ade876d381387b
Michael Bourjac <michael.bourjac@gmail.com>
2023-10-13 19:43:09 +0200

    Refactor the SanityRepository class by making fetchData a generic method

--- src/sanity/sanity.repository.ts
+++ src/sanity/sanity.repository.ts
@@ -14,57 +14,25 @@ export abstract class SanityRepository<T extends z.ZodTypeAny>
 
   getAllDocuments = async () => {
     const query = groq`*[_type == "${this.type}"] | order(_createdAt desc) ${this.projection}`;
-    return this.getMultipleResults(query);
+    return this.fetchData<z.ZodArray<T, 'many'>>(query);
   };
 
   getSingleDocument = async () => {
     const query = groq`*[_type == "${this.type}"]${this.projection}[0]`;
-    return this.getSingleResult(query);
+    return this.fetchData<T>(query);
   };
 
   getNewestDocument = async () => {
     const query = groq`*[_type == "${this.type}"] | order(_createdAt desc) ${this.projection}[0]`;
-    return this.getSingleResult(query);
+    return this.fetchData<T>(query);
   };
 
   getDocumentBySlug = async (slug?: string) => {
     const query = groq`*[_type == "${this.type}" && slug.current == "${slug}"]${this.projection}[0]`;
-    return this.getSingleResult(query);
+    return this.fetchData<T>(query);
   };
 
-  private getMultipleResults = async (query: string): Promise<z.infer<T>[]> => {
-    const multipleResultsSchema = z.object({
-      result: z.array(this.schema),
-    });
-
-    const data = await this.fetchData(query);
-    const parsedData = multipleResultsSchema.safeParse(data);
-
-    if (!parsedData.success) {
-      console.error(this.type, parsedData.error);
-      throw new Error(`Error while validating ${this.type} schema.`);
-    }
-
-    return parsedData.data.result;
-  };
-
-  private getSingleResult = async (query: string): Promise<z.infer<T>> => {
-    const singleResultSchema = z.object({
-      result: this.schema,
-    });
-
-    const data = await this.fetchData(query);
-    const parsedData = singleResultSchema.safeParse(data);
-
-    if (!parsedData.success) {
-      console.error(this.type, parsedData.error);
-      throw new Error(`Error while validating ${this.type} schema.`);
-    }
-
-    return parsedData.data.result;
-  };
-
-  private fetchData = async (query: string): Promise<unknown> => {
+  private fetchData = async <U extends z.ZodTypeAny>(query: string) => {
     const encodedQuery = encodeURIComponent(query);
     const url = `https://${this.PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${this.DATASET}?query=${encodedQuery}`;
     const response = await fetch(url);
@@ -76,6 +44,14 @@ export abstract class SanityRepository<T extends z.ZodTypeAny>
       throw new Error(`Error ${response.status} while fetching Sanity data.`);
     }
 
-    return response.json();
+    const data = await response.json();
+    const parsedData = this.schema.safeParse(data.result);
+
+    if (!parsedData.success) {
+      console.error(this.type, parsedData.error);
+      throw new Error(`Error while validating ${this.type} schema.`);
+    }
+
+    return parsedData.data as z.infer<U>;
   };
 }
